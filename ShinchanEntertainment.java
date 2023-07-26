a5deb8166a0d1f6f48cfd07e9e3562ec8a104a25
V-Malar <128116398+V-Malar@users.noreply.github.com>
2023-07-26 18:21:59 +0530

    Update ShinchanEntertainment.java

--- ShinchanEntertainment.java
+++ ShinchanEntertainment.java
@@ -4,38 +4,34 @@ import java.util.Scanner;
 
 public class ShinchanEntertainment {
 
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		Shiro shiro = new Shiro();
 		Shinchan shinchan = new Shinchan();
 		String inputclassname = sc.next();
-		Things things = (Things)Class.forName(inputclassname).newInstance();
+		Things things = (Things) Class.forName(inputclassname).newInstance();
 		shinchan.playWithShiro(shiro, things);
 	}
 
 }
-class Shiro
-{
-	public void play(Things things) throws PlayExceptions
-	{
+
+class Shiro {
+	public void play(Things things) throws PlayExceptions {
 		things.play();
 	}
 }
-class Shinchan
-{
-	public void playWithShiro(Shiro shiro, Things things)
-	{
-		try
-		{
+
+class Shinchan {
+	public void playWithShiro(Shiro shiro, Things things) {
+		try {
 			shiro.play(things);
-		}
-		catch(PlayExceptions pe)
-		{
+		} catch (PlayExceptions pe) {
 			pe.visit();
 		}
 	}
 }
+
 abstract class Things {
 	abstract public void play() throws PlayExceptions;
 }
@@ -89,70 +85,69 @@ class Song extends Things {
 abstract class PlayExceptions extends Exception {
 	abstract public void visit();
 }
-class Eat extends PlayExceptions
-{
+
+class Eat extends PlayExceptions {
 
 	@Override
 	public void visit() {
 		// TODO Auto-generated method stub
 		new Activity().action(this);
 	}
-	
+
 }
-class Competite extends PlayExceptions
-{
+
+class Competite extends PlayExceptions {
 
 	@Override
 	public void visit() {
 		// TODO Auto-generated method stub
 		new Activity().action(this);
 	}
-	
+
 }
-class Fight extends PlayExceptions
-{
+
+class Fight extends PlayExceptions {
 	@Override
 	public void visit() {
 		// TODO Auto-generated method stub
 		new Activity().action(this);
 	}
 }
-class Draw extends PlayExceptions
-{
+
+class Draw extends PlayExceptions {
 	@Override
 	public void visit() {
 		// TODO Auto-generated method stub
 		new Activity().action(this);
 	}
 }
-class Dance extends PlayExceptions
-{
+
+class Dance extends PlayExceptions {
 	@Override
 	public void visit() {
 		// TODO Auto-generated method stub
 		new Activity().action(this);
 	}
 }
-class Activity
-{
-	public void action(Eat e)
-	{
+
+class Activity {
+	public void action(Eat e) {
 		System.out.println("Shinchan and Shiro eat Chocopie..");
 	}
-	public void action(Competite c)
-	{
+
+	public void action(Competite c) {
 		System.out.println("Shinchan and Shiro competite to Win..");
 	}
-	public void action(Fight f)
-	{
+
+	public void action(Fight f) {
 		System.out.println("Shinchan and Shiro fight for Action Comin..");
 	}
-	public void action(Draw dr)
-	{
+
+	public void action(Draw dr) {
 		System.out.println("Shinchan and Shiro going to Draw..");
 	}
-	public void action(Dance da)
-	{
+
+	public void action(Dance da) {
 		System.out.println("Shinchan and Shiro going to Dance..");
 	}
 }
